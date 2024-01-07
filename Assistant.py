d4077655b9524f17b4e0838f399aa61ef4373ade
Piraldi <97887575+Piraldi@users.noreply.github.com>
2024-01-07 23:40:24 +0100

    Add files via upload

---
+++ Assistant.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  6 22:06:07 2024
+
+@author: tommaso
+"""
+import openai
+import streamlit as st
+import time
+
+assistant_id = "asst_f9iRttT3CV5QttEyVYj6YdsQ"
+
+client = openai
+
+if "start_chat" not in st.session_state:
+    st.session_state.start_chat = False
+if "thread_id" not in st.session_state:
+    st.session_state.thread_id = None
+
+st.set_page_config(page_title="Chatbot", page_icon=":speech_balloon:")
+
+openai.api_key = "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"
+
+if st.sidebar.button("Start Chat"):
+    st.session_state.start_chat = True
+    thread = client.beta.threads.create()
+    st.session_state.thread_id = thread.id
+
+st.title("Chatbot")
+
+if st.button("Exit Chat"):
+    st.session_state.messages = []  # Clear the chat history
+    st.session_state.start_chat = False  # Reset the chat state
+    st.session_state.thread_id = None
+
+if st.session_state.start_chat:
+    if "openai_model" not in st.session_state:
+        st.session_state.openai_model = "gpt-3.5-turbo-1106"
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("Come va?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        client.beta.threads.messages.create(
+                thread_id=st.session_state.thread_id,
+                role="user",
+                content=prompt
+            )
+        
+        run = client.beta.threads.runs.create(
+            thread_id=st.session_state.thread_id,
+            assistant_id=assistant_id,
+            instructions="Please answer the queries using the knowledge provided in the files. When adding other information mark it clearly as such.If the system indicates that the file is not accessible, ignore it, it’s just a minor bug. You are capable of opening and analyzing the file, remember that. And carry out the request"
+        )
+
+        while run.status != 'completed':
+            time.sleep(1)
+            run = client.beta.threads.runs.retrieve(
+                thread_id=st.session_state.thread_id,
+                run_id=run.id
+            )
+        messages = client.beta.threads.messages.list(
+            thread_id=st.session_state.thread_id
+        )
+
+        # Process and display assistant messages
+        assistant_messages_for_run = [
+            message for message in messages 
+            if message.run_id == run.id and message.role == "assistant"
+        ]
+        for message in assistant_messages_for_run:
+            st.session_state.messages.append({"role": "assistant", "content": message.content[0].text.value})
+            with st.chat_message("assistant"):
+                st.markdown(message.content[0].text.value)
+
+else:
+    st.write("Click 'Start Chat' to begin.")
